6535a300f412047f0bd2bd1c5faf4969e9903d41
devopscourses <skydevapp@gmail.com>
2019-07-11 13:12:10 -0400

    logging-1_0

--- create_vm.sh
+++ create_vm.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export GOOGLE_PROJECT=docker-240808
 docker-machine create --driver google \
---google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
---google-machine-type n1-standard-1 \
---google-disk-size 60 \
---google-zone europe-west1-d \
-docker-host
+    --google-machine-image https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts \
+    --google-machine-type n1-standard-1 \
+    --google-open-port 5601/tcp \
+    --google-open-port 9292/tcp \
+    --google-open-port 9411/tcp \
+    logging
